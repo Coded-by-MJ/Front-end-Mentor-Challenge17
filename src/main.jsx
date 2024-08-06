dff33bccb4b29cefc6c22dd537b46afa569e522f
Miracle Ibharokhonre <miracleosemudiahen@hotmail.com>
2024-08-06 21:58:11 +0100

    Final Version

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { QueryClientProvider, QueryClient, } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -12,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}> 
       <App /> 
-      <ReactQueryDevtools /> 
     </QueryClientProvider>
   </React.StrictMode>
 )
